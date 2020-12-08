37fc4151031f1c132e9a3c7aa43e79b2cd682f29
unknown <marzato32@gmail.com>
2020-12-07 23:41:21 -0400

    falta poco para terminarlo listo del todo, pero con fallas

--- src/js/component/Body.js
+++ src/js/component/Body.js
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 
-const Body = () => {
+const Body = props => {
+	const [currentWord, setCurrentWord] = useState("");
+	const [todoList, setTodoList] = useState([]);
+	const addTodoList = event => {
+		if (event.key === "Enter") {
+			console.log(todoList);
+			setTodoList([currentWord, ...todoList]);
+		}
+	};
 	return (
 		<>
 			<div className="Title">
 				<h1>Todo List</h1>
 			</div>
 			<div className="InputText">
-				<input />
-				Escriba su texto aquí
+				<input
+					placeholder="Escriba su texto aquí"
+					type="text"
+					onKeyPress={addTodoList}
+					onChange={event => setCurrentWord(event.target.value)}
+					value={currentWord}
+				/>
+
 				<div className="List">
-					<ul>
-						<li />
-					</ul>
+					{todoList.map((item, index) => {
+						return (
+							<div key={index}>
+								{item}
+								<button
+									id={index}
+									type="button"
+									onClick={event => {
+										console.log(event.target.id);
+									}}>
+									Borrar
+								</button>
+							</div>
+						);
+					})}
 				</div>
 				<div className="item">
-					<h3>items restante</h3>
+					<h3>items restante: {todoList.length} </h3>
 				</div>
 			</div>
 		</>
